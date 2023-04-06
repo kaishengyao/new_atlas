ba4e7e565f0c0f9164063f8c413ac5870ab74966
kaishengyao <kaishengy@gmail.com>
2023-04-05 22:33:15 -0700

    added scripts

---
+++ example_scripts/anli/s2_fine_tune.sh
@@ -0,0 +1,36 @@
+python3 new_atlas/train.py --shuffle --train_retriever \
+                --gold_score_mode=pdist \
+                --use_gradient_checkpoint_reader \
+                --use_gradient_checkpoint_retriever \
+                --precision=bf16 \
+                --temperature_gold=0.01 \
+                --temperature_score=0.01 \
+                --refresh_index=-1 \
+                --query_side_retriever_training \
+                --name=my-anli-2index-train \
+                --per_gpu_batch_size=16 \
+                --per_gpu_embedder_batch_size=16 \
+                --train_data=drive/MyDrive/data/new_atlas/anli_data/train.jsonl \
+                --eval_data=drive/MyDrive/data/new_atlas/anli_data/dev.jsonl \
+                --write_results \
+                --generation_max_length=2 \
+                --reader_model_type=google/flan-t5-small \
+                --text_maxlength=512 \
+                --target_maxlength=5 \
+                --n_context=10 \
+                --total_steps=10000 \
+                --save_freq=500 \
+                --retriever_n_context=10 \
+                --checkpoint_dir=drive/MyDrive/experiments/rag/new_atlas/results \
+                --index_mode=flat \
+                --task=qa \
+                --passages='drive/MyDrive/data/corpora/wiki/enwiki-dec2018/text-list-100-sec.jsonl drive/MyDrive/data/corpora/wiki/enwiki-dec2018/infoboxjsonl' \
+                --save_index_n_shards=2 \
+                --ignore_mismatched_sizes=True \
+                --num_retriever_attention_heads=2 \
+                --retriever_hidden_size=16 \
+                --num_encoder_layers=2 \
+                --num_decoder_layers=2 \
+                --num_retriever_layers=2 \
+                --retriever_hidden_size=16 \
+                --ignore_mismatched_sizes=True 
